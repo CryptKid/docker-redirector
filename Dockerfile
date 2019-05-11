8759c1b27d214f4f995a46baeec8e0c07d92086d
CryptKiddie <cryptkiddie@myerfa.de>
2019-05-11 18:20:31 +0200

    Allow to keep request uri

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM nginx:latest as redirector
 ENV dest="https://github.com/cryptkid/docker-redirector"
 ENV access_log="off"
+ENV keep_uri="false"
 RUN mkdir /redirector
 COPY redirector/ /redirector/
 EXPOSE 80
--- redirector/ENTRYPOINT.sh
+++ redirector/ENTRYPOINT.sh
@@ -8,9 +8,14 @@ exe cp /redirector/redirects.conf /etc/nginx/conf.d/default.conf
 echo "Escaping Vars"
 dest="$(echo "$dest" | sed 's/\//\\\//g')"
 access_log="$(echo "$access_log" | sed 's/\//\\\//g')"
-echo "$dest"
-echo "$access_log"
+echo "Using the following vars:"
+echo "dest: $dest"
+echo "access_log $access_log"
+echo "keep_uri $keep_uri"
 echo "Adding custom settings"
+if [ "$keep_uri" = "true" ]; then
+	dest="$dest\\\$request_uri"
+fi
 exe sed -i "s/\!DEST/$dest/" /etc/nginx/conf.d/default.conf
 exe sed -i "s/\!ACCESS_LOG/$access_log/" /etc/nginx/conf.d/default.conf
 echo "Starting Webserver"
